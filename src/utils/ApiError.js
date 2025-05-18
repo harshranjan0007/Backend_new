c0e5bfeb14efb7df0d08ee8456ce7510180f2601
harshranjan0007 <harshranjan0007@gmail.com>
2025-05-18 15:42:58 +0530

    util code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        massage = "Something went wrong",
+        errors = [],
+        statck = ""
+        ){
+            super(massage)
+            this.statusCode = statusCode
+            this.data = null
+            this.massage= massage
+            this.success = false;
+            this.errors = errors
+
+            if (statck) {
+                this.stack = stack
+            } else {
+                Error.captureStackTrace(this, this.constructor)
+            }
+            
+        }
+}
+
+export { ApiError }
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiRespones {
+    constructor(statusCode , data , massage = "Success"){
+        this.statusCode = statusCode
+        this.data  = data
+        this.massage = massage
+        this.success = statusCode < 400
+    }
+}
