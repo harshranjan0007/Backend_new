39adbebe4858ac77bf9d8256fb28f41bcd60f955
harshranjan0007 <harshranjan0007@gmail.com>
2025-05-19 02:23:56 +0530

    add user and video

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,11 +2,11 @@ const asyncHandler = (requestHandler) => {
     (req, res, next) => {
         Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
-    return 
 
 }
 
 export { asyncHandler }
+
 
 // const asyncHandler = (fn) => async(req , res , next) => {
 //     try {
